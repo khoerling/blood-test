f58d2fa650a97ba2dd499bd4851b6a61bb5618fb
mr. keith <keith@hoerling.com>
2019-08-04 16:59:00 -0700

    refactor: satisfying lerna

--- native/App.tsx
+++ native/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Questionaires } from '../shared/questionnaire'
+import { Questionaires } from 'questions'
 
 export default function App() {
   return (
