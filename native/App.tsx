8e1cb2c24fb858d55e725cdf8906a4dc31b3faf8
mr. keith <keith@hoerling.com>
2019-08-07 17:56:28 -0700

    wip: Example <App /> to read Questionnaire data with types

--- native/App.tsx
+++ native/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Questionaires } from 'questions'
+import { Questionaires, QuestionGroups, Questions } from 'questions'
 
+// a simple App to read & separate our data with types: Questionaires -> Question Group -> Question
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{ Object.keys(Questionaires()) }</Text>
+      {
+        Object.keys(Questionaires()).map(test => (
+          // tests
+          <View style={styles.row}>
+            <Text style={styles.h1}>* {test}</Text>
+            {QuestionGroups(test).map(qg => (
+              // question groups
+              <View style={styles.row}>
+                <Text style={styles.h2}>- {qg}</Text>
+                {Questions(test, qg).map(q => (
+                  // questions
+                  <Text style={[styles.row, styles.question]}>
+                    {q}
+                  </Text>
+                ))}
+              </View>
+            ))}
+          </View>
+        ))
+      }
     </View>
   )
 }
@@ -14,7 +34,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginLeft: 75,
   },
+  row: {
+    flexDirection: 'column',
+  },
+  h1: {
+    fontSize: 20,
+  },
+  h2: {
+    fontSize: 15,
+    marginLeft: 10,
+  },
+  question: {
+    fontSize: 12,
+    marginLeft: 25,
+  }
 })
