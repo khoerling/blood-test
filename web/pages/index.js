9d13e80d25198ec828129143477cd152c10086b3
ewolfe <e@ewolfe.me>
2019-08-10 18:54:49 -0700

    Setup stripe demo flow

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,17 +1,43 @@
 import styled from "styled-components";
-import Head from "next/head";
+import React from "react";
 
 const Wrapper = styled.div`
   background: rgb(249, 198, 172);
   height: 100%;
 `;
 
-function Home() {
-  return (
-    <Wrapper>
-      Welcome to Next.js!
-    </Wrapper>
-  );
+class Home extends React.Component {
+  onCheckout = () => {
+    const stripe = window.Stripe("pk_test_xKDupS5AmFXBdTPfNaaPqfsO00LcCJ9kT1");
+    stripe
+      .redirectToCheckout({
+        items: [{ sku: "sku_FbIhIQx7psdUxU", quantity: 1 }],
+
+        // Do not rely on the redirect to the successUrl for fulfilling
+        // purchases, customers may not always reach the success_url after
+        // a successful payment.
+        // Instead use one of the strategies described in
+        // https://stripe.com/docs/payments/checkout/fulfillment
+        successUrl: window.location.protocol + "//localhost:3000/success",
+        cancelUrl: window.location.protocol + "//localhost:3000/canceled"
+      })
+      .then(function(result) {
+        if (result.error) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer.
+          var displayError = document.getElementById("error-message");
+          displayError.textContent = result.error.message;
+        }
+      });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <button onClick={this.onCheckout}>Buy Complete Blood Count</button>
+      </Wrapper>
+    );
+  }
 }
 
 export default Home;
