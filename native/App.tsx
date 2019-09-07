b4a373a1f4f6b8e3350b528a4778094d6a389d78
mr. keith <keith@hoerling.com>
2019-09-06 22:39:23 -0700

    wip: homescreen showcasing subscriptions

--- native/App.tsx
+++ native/App.tsx
@@ -1,20 +1,40 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StatusBar } from 'react-native'
 // import { Questionaires, QuestionGroups, Questions } from 'questions'
 import cssta from 'cssta/native'
 import { BottomCarousel } from './src/components/BottomCarousel'
 
 const App = cssta(View)`
-  flex: 1;
-  justifyContent: center;
-  alignItems: center;
-  marginTop: 40;
-`,
+    flex: 1;
+    marginTop: 40;
+  `,
+  Head = cssta(View)`
+    marginTop: 10px;
+  `,
+  H1 = cssta(Text)`
+    lineHeight: 24rem;
+    margin: 0 30px 25px;
+    fontSize: 18rem;
+  `,
+  H2 = cssta(Text)`
+    textAlign: justify;
+    lineHeight: 24rem;
+    margin: 0 30px;
+    fontSize: 13rem;
+  `,
   entries = require('./src/static/fake_tests.json')
 
 export default () => (
   <App>
-    <Text>Select a Test</Text>
+    <StatusBar hidden={true} />
+    <Head>
+      <H2>UPCOMING:</H2>
+      <H1>Tomorrow @ Noon for Lipid Panel</H1>
+    </Head>
+    <H2>
+      You've Come to the Right Place, Friend. Choose a Test and Prolong Your
+      Health & Lifestyle:
+    </H2>
     <BottomCarousel
       style={{
         backgroundColor: 'blue',
