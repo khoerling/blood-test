744f83bf700d20c18385e8d77fb1986bd891ae2d
ewolfe <e@ewolfe.me>
2019-09-08 15:29:18 -0700

    normalize css and make background yellow

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -1,9 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
   render() {
@@ -11,14 +11,18 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <script src="https://js.stripe.com/v3/" />
+          <link
+            href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,43 +1,37 @@
-import styled from "styled-components";
-import React from "react";
-
-const Wrapper = styled.div`
-  background: rgb(249, 198, 172);
-  height: 100%;
-`;
+import React from 'react'
 
 class Home extends React.Component {
   onCheckout = () => {
-    const stripe = window.Stripe("pk_test_xKDupS5AmFXBdTPfNaaPqfsO00LcCJ9kT1");
+    const stripe = window.Stripe('pk_test_xKDupS5AmFXBdTPfNaaPqfsO00LcCJ9kT1')
     stripe
       .redirectToCheckout({
-        items: [{ sku: "sku_FbIhIQx7psdUxU", quantity: 1 }],
+        items: [{ sku: 'sku_FbIhIQx7psdUxU', quantity: 1 }],
 
         // Do not rely on the redirect to the successUrl for fulfilling
         // purchases, customers may not always reach the success_url after
         // a successful payment.
         // Instead use one of the strategies described in
         // https://stripe.com/docs/payments/checkout/fulfillment
-        successUrl: window.location.protocol + "//localhost:3000/success",
-        cancelUrl: window.location.protocol + "//localhost:3000/canceled"
+        successUrl: window.location.protocol + '//localhost:3000/success',
+        cancelUrl: window.location.protocol + '//localhost:3000/canceled'
       })
       .then(function(result) {
         if (result.error) {
           // If `redirectToCheckout` fails due to a browser or network
           // error, display the localized error message to your customer.
-          var displayError = document.getElementById("error-message");
-          displayError.textContent = result.error.message;
+          var displayError = document.getElementById('error-message')
+          displayError.textContent = result.error.message
         }
-      });
-  };
+      })
+  }
 
   render() {
     return (
-      <Wrapper>
+      <div>
         <button onClick={this.onCheckout}>Buy Complete Blood Count</button>
-      </Wrapper>
-    );
+      </div>
+    )
   }
 }
 
-export default Home;
+export default Home
