9fd99129123f311bcdc712861608ffc9cf770a2a
ewolfe <e@ewolfe.me>
2019-09-22 21:01:44 -0700

    setup https server for localhost

---
+++ web/server.js
@@ -0,0 +1,24 @@
+const { createServer } = require('https')
+const { parse } = require('url')
+const { readFileSync } = require('fs')
+const next = require('next')
+
+const port = 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const httpsOptions = {
+  key: readFileSync('./certificates/localhost-key.pem'),
+  cert: readFileSync('./certificates/localhost-cert.pem')
+}
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on https://localhost:${port}`)
+  })
+})
